bfc2b106c6130a743a9e82ea72cac2002eedec6a
Terry N. Brown <terry_n_brown@yahoo.com>
2017-01-17 14:34:18 -0600

    add README, links

---
+++ README.md
@@ -0,0 +1,3 @@
+Simple javascript FOV calc.
+
+View it [running on line](https://tbnorth.github.io/fov_simple/).
--- make_static.py
+++ make_static.py
@@ -1,5 +1,15 @@
+"""
+make_static.py - replace parts of index.html with inline
+CSS / javascript
+
+Terry N. Brown, terrynbrown@gmail.com, Tue Jan 17 14:28:44 2017
+"""
+
 import urllib2
 
+# from is the text to replace in index.html
+# to is either the URL for the replacement or ""
+# in is the wrapper for to, like "<script>%s</script>"
 from_to_in = [
     ("""<link rel="stylesheet"
  href="https://code.jquery.com/ui/1.12.1/themes/black-tie/jquery-ui.css"/>""",
@@ -10,7 +20,7 @@ from_to_in = [
     ("""<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>""",
      "https://code.jquery.com/ui/1.12.1/jquery-ui.min.js", "<script>%s</script>"),
     ("""<p>
-<a href='.' download='fov_simple.html'>Download this page
+<a href='fov_simple.html' download='fov_simple.html'>Download this page
 for offline use.</a></p>""", "", "%s"),
 ]
 
@@ -20,3 +30,4 @@ for from_, to, in_ in from_to_in:
         to = urllib2.urlopen(to).read()
     src = src.replace(from_, in_ % to)
 open("fov_simp.html", 'w').write(src)
+
