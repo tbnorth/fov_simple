ea3b0fb4d2f586cc7f4eb7893efd1778f2e79bb5
Terry N. Brown <terry_n_brown@yahoo.com>
2017-01-17 15:41:00 -0600

    fix touch interface

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Simple javascript FOV calc.
 
 View it [running on line](https://tbnorth.github.io/fov_simple/).
+
--- make_static.py
+++ make_static.py
@@ -12,13 +12,16 @@ import urllib2
 # in is the wrapper for to, like "<script>%s</script>"
 from_to_in = [
     ("""<link rel="stylesheet"
- href="https://code.jquery.com/ui/1.12.1/themes/black-tie/jquery-ui.css"/>""",
-     "https://code.jquery.com/ui/1.12.1/themes/black-tie/jquery-ui.css",
-     "<style>%s</style>"),
-    ("""<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>""",
-     "https://code.jquery.com/jquery-3.1.1.min.js", "<script>%s</script>"),
-    ("""<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>""",
-     "https://code.jquery.com/ui/1.12.1/jquery-ui.min.js", "<script>%s</script>"),
+ href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css"/>""",
+     "https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css",
+     "<style>\n%s\n</style>"),
+    ("""<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>""",
+     "https://code.jquery.com/jquery-2.2.4.min.js",
+     "<script>\n%s\n</script>"),
+    ("""<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>""",
+     # "https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js",
+     "https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.js",
+     "<script>\n%s\n</script>"),
     ("""<p>
 <a href='fov_simp.html' download='fov_simp.html'>Download this page
 for offline use.</a></p>""", "", "%s"),
